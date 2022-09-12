11b239e16842ed61f718f22dc90a71e93822856a
何方 <he529564582@163.com>
2022-09-12 22:54:50 +0800

    增加配置webpack

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,16 @@
 import "./style/index.less"
+import Food from  "./modules/Food"
+import ScorePanel from "./modules/ScorePanel"
 
-class Food { 
-  element: HTMLElement;
-  constructor() { 
-    this.element = document.getElementById("food")!; 
-  }
-  get X() { 
-    return this.element.offsetLeft
-  }
-  get Y() { 
-    return this.element.offsetTop
-  }
-  change() {
-    let top =  Math.round(Math.random() * 29)*10
-    let left = Math.round(Math.random() * 29) * 10
-    this.element.style.left = left + "px"
-    this.element.style.top = top + "px"
-  }
-}
+
 
 const food = new Food()
 console.log(food.X, food.Y)
 food.change()
 console.log(food.X, food.Y)
 
+
+const scorePanel = new ScorePanel(100, 10)
+for (let i = 10; i < 100; i++) { 
+  scorePanel.addScore()
+}
